74615587b7b3ef168fb672840eb9570a97f638e5
Yasmeen Al Shakarchi <yalshakarchi@ucsd.edu>
2026-03-13 12:07:34 -0700

    add c4

---
+++ class04/class04.R
@@ -0,0 +1,8 @@
+#' ---
+#' title: "class 4 Intro to R"
+#' author: "Yasmeen Al Shakarchi"
+#' date: "Jan 16 2026"
+#' ---
+x <- 1:50
+plot(x)
+plot(x, sin(x),typ="l", col="blue", lwd=3, xlab="is it lunch time")
